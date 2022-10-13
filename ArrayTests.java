0b23dd212cb234a1c82ca2eeba7aca2058ca0e7d
SaltySharky <thunguyenminh@gmail.com>
2022-10-13 13:08:24 -0700

    fixed reverseInPlace. added more tests.

--- ArrayTests.java
+++ ArrayTests.java
@@ -17,6 +17,10 @@ public class ArrayTests {
     int[] input3 = { 1, 2, 3, 4};
     ArrayExamples.reverseInPlace(input3);
     assertArrayEquals(new int[]{4, 3, 2, 1}, input3);
+
+    int[] input4 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    ArrayExamples.reverseInPlace(input4);
+    assertArrayEquals(new int[]{8, 7, 6, 5, 4, 3, 2, 1, 0}, input4);
 	}
 
 
