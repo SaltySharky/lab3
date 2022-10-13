16e7dd7e74d689e6f021b04d974462b00b08f122
SaltySharky <thunguyenminh@gmail.com>
2022-10-13 13:35:01 -0700

    fixed reversed.

--- ArrayTests.java
+++ ArrayTests.java
@@ -26,7 +26,10 @@ public class ArrayTests {
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {0, 1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1, 0}, ArrayExamples.reversed(input1));
+
+    int[] input2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    assertArrayEquals(new int[]{8, 7, 6, 5, 4, 3, 2, 1, 0}, ArrayExamples.reversed(input2));
   }
 }
