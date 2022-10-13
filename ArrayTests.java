81530e264a7893c41e1c7f61247de6684ff2a757
SaltySharky <thunguyenminh@gmail.com>
2022-10-13 14:22:49 -0700

    added testAverageWithoutLowest

--- ArrayTests.java
+++ ArrayTests.java
@@ -32,4 +32,17 @@ public class ArrayTests {
     int[] input2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
     assertArrayEquals(new int[]{8, 7, 6, 5, 4, 3, 2, 1, 0}, ArrayExamples.reversed(input2));
   }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {0, 1, 2, 3};
+    double expected = 2;
+    // last argument is delta, the difference margin between the first two args
+    assertEquals(expected, ArrayExamples.averageWithoutLowest(input1), 0);
+
+    double[] input2 = {1, 1, 1, 2, 3, 3};
+    expected = 2.0;
+    // last argument is delta, the difference margin between the first two args
+    assertEquals(expected, ArrayExamples.averageWithoutLowest(input2), 0);
+  }
 }
